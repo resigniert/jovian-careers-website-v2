9907fb01dbb8f97f5511add526bd81fded8091ad
resigniert <143401638+resigniert@users.noreply.github.com>
2023-08-30 15:04:27 +0000

    DB Update

--- database.py
+++ database.py
@@ -1,8 +1,7 @@
-# Username: 6bqowpku8cuskgx8f4xq
-# Password: pscale_pw_zDDX6altrNPd4mW7CyNWJtZV1470Q4VmmALrCbDqhGm
-#
-#
-#
+# DB_HOST=aws.connect.psdb.cloud
+# DB_USERNAME=6bqowpku8cuskgx8f4xq
+# DB_PASSWORD=pscale_pw_zDDX6altrNPd4mW7CyNWJtZV1470Q4VmmALrCbDqhGm
+# DB_NAME=joviancareers
 
 import sqlalchemy
 from sqlalchemy import create_engine
